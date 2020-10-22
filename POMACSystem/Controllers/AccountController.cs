a587daecfa793d1ce1025c2e10b27c4b0b9e58d0
Canjetan <canjetanngahu@gmail.com>
2020-10-22 09:02:35 +0300

    tabs added

--- POMACSystem/Controllers/AccountController.cs
+++ POMACSystem/Controllers/AccountController.cs
@@ -403,7 +403,7 @@ namespace POMACSystem.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Login", "Account");
         }
 
         //
